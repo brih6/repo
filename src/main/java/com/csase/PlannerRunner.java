2cb21e08e1d0bf1f1fe4680efa9db3ff0cfc095c
brih6 <hansebri001@hsestudents.org>
2024-05-10 11:29:32 -0400

    fixed student.java

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -5,9 +5,11 @@ public class PlannerRunner {
 
         // Create a new student
         Student jada = new Student();
+        Student bri = new Student();
 
         // Gets the student's choice until the student exits
         jada.getChoice();
+        bri.getChoice();
 
     }
 }
